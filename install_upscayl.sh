553de279b22214bc47c9aaa852fa8c80a775c7ad
Phachayawat-[Pchpch11] <patchayawat54@gmail.com>
2024-01-27 12:13:50 +0700

    add insall_sh

---
+++ install_upscayl.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Please follow step below
+
+
+# To install and build Upscayl, you can follow the steps below:
+# Once you have completed these steps, you should be able to use Upscayl on your machine.
+
+# Clone the Upscayl repository from GitHub using the following command at the directory where you want to install Upscayl
+git clone https://github.com/upscayl/upscayl.git
+
+# Navigate to the upscayl directory using the following command:
+cd upscayl
+
+# Install the required dependencies using the following command:
+npm install
+
+# Build the application using the following command:
+npm run build
+
+echo "🤘🤘🤘🤘 Build The Upscayl application was successfull 🤘🤘🤘"
+echo "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 🚀🚀🚀🚀🚀🚀🚀"
+echo "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  going to Run \"npm start\" for open Upscayl app"
+echo "🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 🚀🚀🚀🚀🚀🚀🚀"
+
+# Start the application using the following command:
+npm start
